e801da2a106f056ef54a3c38d00d2422b38eef2c
Jonas Ampferl <Jonas@ampferl.com>
2021-03-12 17:43:39 +0100

    Refactored drawing to the kernel.cpp

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,4 +1,22 @@
 
-extern "C" int _start(){
-	return 123;
+typedef unsigned long long size_t;
+
+typedef struct {
+    void* BaseAddress;
+    size_t BufferSize;
+    unsigned int Width;
+    unsigned int Height;
+    unsigned int PixelsPerScanline;
+} Framebuffer;
+
+extern "C" void _start(Framebuffer *framebuffer){
+
+    unsigned int y = 50;
+    unsigned int BBP = 4;
+
+    for(unsigned int x = 0; x < framebuffer->Width / 2 * BBP; x+=BBP){
+        *(unsigned int*)(x + (y * framebuffer->PixelsPerScanline * BBP) + framebuffer->BaseAddress) = 0xFF00FFFF;
+    }
+
+    return ;
 }
