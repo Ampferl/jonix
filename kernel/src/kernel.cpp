1acc949e0034d5ff96cffe2f52bd342ecf53111d
Jonas Ampferl <Jonas@ampferl.com>
2021-03-14 12:24:38 +0100

    Refactored and tidying kernel.cpp up

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,59 +1,15 @@
-#include <stdint.h>
-#include "BasicRenderer.h"
-#include "efiMemory.h"
-#include "memory.h"
-#include "Bitmap.h"
-#include "paging/PageFrameAllocator.h"
-#include "paging/PageMapIndexer.h"
-#include "cstr.h"
-#include "paging/paging.h"
-#include "paging/PageTableManager.h"
-
-struct BootInfo{
-    Framebuffer *framebuffer;
-    PSF1_FONT *psf1_Font;
-    EFI_MEMORY_DESCRIPTOR *mMap;
-    uint64_t mMapSize;
-    uint64_t mMapDescSize;
-};
-
-extern uint64_t _KernelStart;
-extern uint64_t _KernelEnd;
+#include "kernelUtil.h"
 
 extern "C" void _start(BootInfo *bootInfo){
 
+    KernelInfo kernelInfo = InitializeKernel(bootInfo);
+    PageTableManager* pageTableManager = kernelInfo.pageTableManager;
+
     BasicRenderer newRenderer(bootInfo->framebuffer, bootInfo->psf1_Font);
-
-    uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescSize;
-    GlobalAllocator = PageFrameAllocator();
-    GlobalAllocator.ReadEFIMemoryMap(bootInfo->mMap, bootInfo->mMapSize, bootInfo->mMapDescSize);
-
-    uint64_t kernelSize = (uint64_t)&_KernelEnd - (uint64_t)&_KernelStart;
-    uint64_t kernelPages = (uint64_t)kernelSize / 4096 + 1;
-
-    GlobalAllocator.LockPages(&_KernelStart, kernelPages);
-
-    PageTable* PML4 = (PageTable*)GlobalAllocator.RequestPage();
-    memset(PML4, 0, 0x1000);
-
-    PageTableManager pageTableManager(PML4);
-
-    for(uint64_t t = 0; t < GetMemorySize(bootInfo->mMap, mMapEntries, bootInfo->mMapDescSize); t+=0x1000){
-        pageTableManager.MapMemory((void*)t, (void*)t);
-    }
-
-    uint64_t fbBase = (uint64_t)bootInfo->framebuffer->BaseAddress;
-    uint64_t fbSize = (uint64_t)bootInfo->framebuffer->BufferSize + 0x1000;
-    GlobalAllocator.LockPages((void*)fbBase, fbSize / 0x1000 + 1);
-    for(uint64_t t = fbBase; t < fbBase + fbSize; t+=4096){
-        pageTableManager.MapMemory((void*)t, (void*)t);
-    }
-
-    asm("mov %0, %%cr3" : : "r" (PML4));
 
     memset(bootInfo->framebuffer->BaseAddress, 0, bootInfo->framebuffer->BufferSize);
 
-    pageTableManager.MapMemory((void*)0x600000000, (void*)0x8000);
+    pageTableManager->MapMemory((void*)0x600000000, (void*)0x8000);
     uint64_t* test = (uint64_t*)0x600000000;
     *test = 26;
 
